f262434f5ddac06e7b7c71457f9b59828e72134d
jae <goawmfhfl1@naver.com>
2022-05-31 07:24:53 +0900

    Add: Recoil 사용해서 장바구니 Mock API 만들기

--- shopping-mall/src/components/product/ProductItem.tsx
+++ shopping-mall/src/components/product/ProductItem.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { Product } from "../../graphql/products";
+import { cartItemSelector } from "../../recoils/cart";
 
 const ProductItem = ({
     id,
@@ -9,6 +11,9 @@ const ProductItem = ({
     description,
     createdAt,
 }: Product) => {
+    const [cartAmount, setCartAmount] = useRecoilState(cartItemSelector(id));
+    const addToCart = () => setCartAmount((prev) => (prev || 0) + 1);
+
     return (
         <li className="products-item">
             <Link to={`/products/${id}`}>
@@ -16,6 +21,10 @@ const ProductItem = ({
                 <img src={imageUrl} className="image" />
                 <span className="price">{price}</span>
             </Link>
+            <button className="add-cart" onClick={addToCart}>
+                장바구니 담기
+            </button>
+            <span>{cartAmount || 0}</span>
         </li>
     );
 };
